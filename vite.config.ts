c5c358413c98c159cee5f2cb0ea26737fe8857a9
tayasarbhat <tayasarbhat@gmail.com>
2024-11-13 09:34:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/quizs/',
+  base: '/tabs/', // Replace 'your-repo-name' with the actual repository name
   plugins: [react()],
-  envDir: '.',
 });
